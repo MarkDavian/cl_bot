19593732900f7ea30066c2846eac5ec433a220f9
Nico Dillinger <writebutread@outlook.com>
2025-04-02 17:35:11 +0300

    .

--- app/telegram/handlers/commands/menu/callback_handler.py
+++
@@ -1,9 +0,0 @@
-from .subjects.export import reg_export_callback
-from .subjects.query.query_menu import reg_query_main_callback
-from .subjects.espresso_machine_health import reg_espresso_health_main_callback
-
-
-def reg_callback_menu(dp):
-    reg_export_callback(dp)
-    reg_query_main_callback(dp)
-    reg_espresso_health_main_callback(dp)
